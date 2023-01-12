ad73cc02eaed2a8b892c8b398d940b2f36eb86b2
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-12 19:49:18 +0200

    mongo connected, added middlewares

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
 const start = async () => {
   try {
+    await mongoose.connect(process.env.DB_URI);
     app.listen(PORT, console.log(`server started at ${PORT} PORT`));
   } catch (error) {
     console.log(error);
