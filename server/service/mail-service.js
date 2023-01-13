d0616e329646980bc8a953583cd2a5fb04b9b21d
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-13 14:51:47 +0200

    _

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -1,5 +1,30 @@
+const nodemailer = require("nodemailer");
+
 class MailService {
-  async sendActivationMail(to, link) {}
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "6728c3fe8e3d7e",
+        pass: "93f9a9be93d8c2"
+      }
+    });
+  }
+  async sendActivationMail(to, link) {
+    await this.transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject: "Acc activation " + process.env.API_URL,
+      text: "",
+      html: `
+          <div>
+            <h1>Follow link to activate acc</h1>
+            <a href="${link}">${link}</a>
+          </div>
+        `,
+    });
+  }
 }
 
 module.exports = new MailService();
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -22,7 +22,7 @@ class UserService {
       activationLink,
     });
 
-    await mailService.sendActivationMail(email, activationLink);
+    await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
 
     const userDto = new UserDto(user);
     const tokens = tokenService.generateTokens({ ...userDto });
