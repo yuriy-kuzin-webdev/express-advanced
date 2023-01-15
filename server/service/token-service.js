eb6d1e6f4172fe51a55223d8bbd1914b68ba519d
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-16 00:05:46 +0200

    added refresh token route

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -13,6 +13,24 @@ class TokenService {
     return { accessToken, refreshToken };
   }
 
+  validateAccessToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+      return userData;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  validateRefreshToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+      return userData;
+    } catch (error) {
+      return null;
+    }
+  }
+
   async saveToken(userId, refreshToken) {
     const tokenData = await tokenModel.findOne({ user: userId });
     if (tokenData) {
@@ -26,6 +44,11 @@ class TokenService {
 
   async removeToken(refreshToken) {
     const tokenData = await tokenModel.deleteOne({ refreshToken });
+    return tokenData;
+  }
+
+  async findToken(refreshToken) {
+    const tokenData = await tokenModel.findOne({ refreshToken });
     return tokenData;
   }
 }
