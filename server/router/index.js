242f92d4e6f43cc6f3dfbe43fe439831763f8046
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-15 18:08:22 +0200

    updated router, installed validator

--- server/router/index.js
+++ server/router/index.js
@@ -6,7 +6,7 @@ router.post("/registration", userController.registration);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 router.get("/activate/:link", userController.activate);
-router.post("/refresh", userController.refresh);
-router.post("/users", userController.getUsers);
+router.get("/refresh", userController.refresh);
+router.get("/users", userController.getUsers);
 
 module.exports = router;
