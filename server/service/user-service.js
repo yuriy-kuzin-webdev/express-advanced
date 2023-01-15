e99543f04cef3f7ccc0d8a4aed396a5756f7b036
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-15 18:21:31 +0200

    added login to user service

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -46,6 +46,21 @@ class UserService {
     user.isActivated = true;
     await user.save();
   }
+
+  async login(email, password) {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      throw ApiError.BadRequest("No user with provided email");
+    }
+    const isPassEquals = await bcrypt.compare(password, user.password);
+    if (!isPassEquals) {
+      throw ApiError.BadRequest("Wrong password");
+    }
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens(userDto);
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return { ...tokens, user: userDto };
+  }
 }
 
 module.exports = new UserService();
