f366f78c47942d27322ff907154bbcc91c2a5983
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-16 23:13:39 +0200

    added mobx store

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,27 @@
+import React, { FC, useState } from "react";
+
+const LoginForm: FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  return (
+    <div>
+      <input
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        type="text"
+        placeholder="Email"
+      />
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        type="password"
+        placeholder="password"
+      />
+      <button>Login</button>
+      <button>Registration</button>
+    </div>
+  );
+};
+
+export default LoginForm;
