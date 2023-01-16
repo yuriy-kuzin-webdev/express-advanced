802a5112e70fc82473db6789ae9a6bac0ebad750
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-16 23:39:31 +0200

    updated login form

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,3 +1,4 @@
+import { observer } from "mobx-react-lite";
 import React, { FC, useState, useContext } from "react";
 import { Context } from "../index";
 const LoginForm: FC = () => {
@@ -28,4 +29,4 @@ const LoginForm: FC = () => {
   );
 };
 
-export default LoginForm;
+export default observer(LoginForm);
