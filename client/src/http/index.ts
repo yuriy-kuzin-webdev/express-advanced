bc92ecade6c5ae618efa9f60c628105f866a0fbe
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-18 20:31:23 +0200

    added refresh interceptor

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { AuthResponse } from "../models/response/AuthResponse";
 
 export const API_URL = "http://localhost:7000/api";
 
@@ -12,5 +13,25 @@ $api.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
   return config;
 });
+
+$api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status == 401) {
+      try {
+        const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {
+          withCredentials: true,
+        });
+        localStorage.setItem("token", response.data.accessToken);
+        return $api.request(originalRequest);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+);
 
 export default $api;
