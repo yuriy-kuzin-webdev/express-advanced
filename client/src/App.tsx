838cf0fd4fbbc5797eeb7f1e0ca71c7627e54f60
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-18 20:19:16 +0200

    added get users

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,31 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { Context } from "./index";
 import LoginForm from "./components/LoginForm";
 import { observer } from "mobx-react-lite";
+import { IUser } from "./models/IUser";
+import UserService from "./services/UserService";
 
 function App() {
   const { store } = useContext(Context);
+  const [users, setUsers] = useState<IUser[]>([]);
   useEffect(() => {
     if (localStorage.getItem("token")) {
       store.checkAuth();
     }
   }, []);
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (store.isLoading) {
+    return <div>Loading ...</div>;
+  }
 
   if (!store.isAuth) {
     return <LoginForm />;
@@ -21,6 +37,12 @@ function App() {
         {store.isAuth ? `Authorized ${store.user.email}` : "Not Authorized"}
       </h1>
       <button onClick={() => store.logout()}>Logout</button>
+      <div>
+        <button onClick={getUsers}>Get users</button>
+      </div>
+      {users.map((user) => (
+        <div key={user.email}>{user.email}</div>
+      ))}
     </div>
   );
 }
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -8,6 +8,7 @@ import AuthService from "../services/AuthService";
 export default class Store {
   user = {} as IUser;
   isAuth = false;
+  isLoading = false;
 
   constructor() {
     makeAutoObservable(this);
@@ -19,6 +20,10 @@ export default class Store {
 
   setUser(user: IUser) {
     this.user = user;
+  }
+
+  setLoading(bool: boolean) {
+    this.isLoading = bool;
   }
 
   async login(email: string, password: string) {
@@ -55,6 +60,7 @@ export default class Store {
   }
 
   async checkAuth() {
+    this.setLoading(true);
     try {
       const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {
         withCredentials: true,
@@ -64,6 +70,8 @@ export default class Store {
       this.setUser(response.data.user);
     } catch (e: any) {
       console.log(e.response?.data?.message);
+    } finally {
+      this.setLoading(false);
     }
   }
 }
